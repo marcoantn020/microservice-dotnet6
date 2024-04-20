3c18bba5df2725d95c16018c9ece05807c0fece5
Marco Antonio <marcoantn020@gmail.com>
2024-04-20 20:57:49 -0300

    first project

---
+++ RestWithApiASPNET/RestWithApiASPNET/Services/IPersonService.cs
@@ -0,0 +1,12 @@
+﻿using RestWithApiASPNET.Models;
+
+namespace RestWithApiASPNET.Services;
+
+public interface IPersonService
+{
+    Person Create(Person person);
+    Person FindById(long id);
+    List<Person> FindAll();
+    Person Update(Person person);
+    void Delete(long id);
+}
---
+++ RestWithApiASPNET/RestWithApiASPNET/Services/Implementations/PersonServiceImplementation.cs
@@ -0,0 +1,62 @@
+﻿using RestWithApiASPNET.Models;
+
+namespace RestWithApiASPNET.Services.Implementations;
+
+public class PersonServiceImplementation : IPersonService
+{
+    private volatile int count;
+
+    public Person Create(Person person)
+    {
+        return person;
+    }
+
+    public void Delete(long id)
+    {
+        
+    }
+
+    public List<Person> FindAll()
+    {
+        List<Person> persons = new();
+        for (int i = 0; i < 8; i++)
+        {
+            Person person = MockPerson(i);
+            persons.Add(person);
+        }
+        return persons;
+    }
+
+    public Person FindById(long id)
+    {
+        return new Person { 
+            Id = incrementAndGet(),
+            FirstName = "Marco",
+            LastName = "Antonio",
+            Address = "Vera Cruz - Sao Paulo - Brasil",
+            Gender = "Male"
+        };
+    }
+
+    public Person Update(Person person)
+    {
+        return person;
+    }
+
+    private Person MockPerson(int i)
+    {
+        return new Person
+        {
+            Id = incrementAndGet(),
+            FirstName = "Person Name" + i,
+            LastName = "Person LastName" + i,
+            Address = "Some Address" + i,
+            Gender = "Male"
+        };
+    }
+
+    private long incrementAndGet()
+    {
+        return Interlocked.Increment(ref count);
+    }
+}
